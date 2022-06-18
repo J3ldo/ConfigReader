6b974ccf6b7c1a293882f32173e94080af31fadc
J3ldo <97303347+J3ldo@users.noreply.github.com>
2022-06-18 19:49:16 +0200

    Add the code

--- ConfigReader.py
+++ ConfigReader.py
@@ -1,1 +1,16 @@
+class Configreader:
+    def __init__(self, splitwith="="):
+        self.splitwith = splitwith
 
+    def readfile(self, filepath):
+        with open(filepath, 'r') as f:
+            filecontents = [i.strip('\n') for i in f.readlines()]
+
+        out = {}
+        for line in filecontents:
+            try:
+                out[line.split('=')[0]] = line.split('=')[1]
+            except IndexError:
+                pass
+
+        return out
